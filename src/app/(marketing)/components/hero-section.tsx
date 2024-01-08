5bc6e5dc0b3dcae3b1dfbe7550556996c0199424
Syed Muzamil <smmhd121@gmail.com>
2024-01-08 15:11:35 +0530

    New landing page design

--- src/app/(marketing)/components/hero-section.tsx
+++ src/app/(marketing)/components/hero-section.tsx
@@ -1,35 +1,41 @@
 import React from "react";
-import appearance from "@/assets/images/appearance.png";
-import Image from "next/image";
+import { Playfair_Display } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Balancer from "react-wrap-balancer";
+
+const playfaire = Playfair_Display({ subsets: ["latin"] });
 
 export const HeroSection = () => {
   return (
-    <section className="bg-brand py-16">
-      <div className="container flex justify-between">
-        <div>
-          <div className="mb-12">
-            <h1 className="text-2xl md:text-4xl text-white font-bold max-w-xl mb-6">
-              Supercharge Your Launch: Turbo-Boost Early Signups with
-              WaitwiseHub!
-            </h1>
-            <p className="text-xl text-white max-w-3xl">
+    <section className="-bg-brand/60 py-16">
+      <div className="container flex flex-col items-center gap-5 max-w-[64rem]">
+        <div className="mb-12 flex flex-col items-center text-center gap-5">
+          <h1
+            className={cn(
+              "text-4xl font-extrabold tracking-light sm:text-5xl md:text-6xl lg:text-7xl text-mantis-800",
+              playfaire.className
+            )}
+          >
+            {/* Supercharge Your Launch: Turbo-Boost Early Signups with
+              WaitwiseHub! */}
+            <Balancer>WaitWiseHub - Supercharge Your Launch</Balancer>
+          </h1>
+          <p className="text-mantis-800 max-w-[42rem] leading-normal text-muted-foreground sm:text-sm md:text-xl sm:leading-0">
+            <Balancer>
               Transform Waiting into Winning – Elevate Your Startup! 🚀 Join
               WaitwiseHub, the Ultimate Waitlist Platform. Focus on Perfecting
               Your Product, Not Just Collecting Signups!
-            </p>
-          </div>
-          <div className="flex gap-4 md:gap-8">
-            <button className="bg-brand-dark shadow-md rounded-full py-2 px-3 md:px-6 text-white font-medium">
-              Build your waitlist
-            </button>
-            <button className="bg-lime-100 rounded-full py-2 px-3 md:px-6 text-brand-dark font-medium">
-              View demo
-            </button>
-          </div>
+            </Balancer>
+          </p>
         </div>
-        {/* <div>
-          <Image src={appearance} alt="Apperance Image" />
-        </div> */}
+        <div className="flex gap-4 md:gap-8">
+          <button className="bg-mantis-900 hover:bg-mantis-950 transition-colors shadow-md rounded-full py-3 px-4 md:py-3 md:px-6 text-white font-medium">
+            Build your waitlist
+          </button>
+          <button className="bg-mantis-100 hover:bg-mantis-200 transition-colors rounded-full py-3 px-4 md:py-3 md:px-6 text-brand-dark font-medium">
+            View demo
+          </button>
+        </div>
       </div>
     </section>
   );
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -2,44 +2,157 @@ import { PackageCheck } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { HeroSection } from "./components/hero-section";
+import Balancer from "react-wrap-balancer";
+import customise from '@/assets/images/appearance.png'
+import Image from 'next/image'
+import { Playfair_Display } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { url } from "@/lib/constants";
+
+const playfaire = Playfair_Display({ subsets: ["latin"] });
 
 const Page = () => {
   return (
     <React.Fragment>
-      <header className="bg-brand py-4">
+      <header className="bg-mantis-900 py-4">
         <div className="container flex justify-between items-center">
           <div className="flex items-center md:gap-2">
             <PackageCheck className="text-mantis-50 text-5xl" />
-            <span className="text-xl md:text-2xl text-mantis-50 font-bold">waitwisehub</span>
+            <span className="text-xl md:text-2xl text-mantis-50 font-bold">
+              waitwisehub
+            </span>
           </div>
-          <div className="hidden md:block rounded-full px-6 py-2 border border-mantis-50/90">
+          <div className="hidden lg:block rounded-full px-6 py-2 border border-mantis-50/90">
             <div className="flex items-center gap-6">
-            <a href="#" className="text-mantis-50">Product</a>
-            <a href="#" className="text-mantis-50">Pricing</a>
-            <a href="#" className="text-mantis-50">Company</a>
-            <a href="#" className="text-mantis-50">Blog</a>
-            <a href="#" className="text-mantis-50">Changelog</a>
+              <a href="#" className="text-mantis-50">
+                Product
+              </a>
+              <a href="#" className="text-mantis-50">
+                Pricing
+              </a>
+              <a href="#" className="text-mantis-50">
+                Company
+              </a>
+              <a href="#" className="text-mantis-50">
+                Blog
+              </a>
+              <a href="#" className="text-mantis-50">
+                Changelog
+              </a>
             </div>
           </div>
           <div className="flex gap-3 items-center">
-            <Link href="/signin" className="text-white">Login</Link>
+            <Link href="/signin" className="text-white hidden md:block">
+              Login
+            </Link>
             <Link href="/signup">
-                <button className="rounded-full py-2 px-3 md:px-6 text-brand-dark bg-mantis-50">Get started</button>
+              <button className="rounded-full py-2 px-3 md:px-6 text-brand-dark bg-mantis-50">
+                Get started
+              </button>
             </Link>
           </div>
         </div>
       </header>
       <HeroSection />
-      {/* <section className="container py-16">
-        <div className="flex flex-col items-center justify-center">
-            <h2 className="text-3xl font-bold mb-4">Features</h2>
-            <p className="text-xl max-w-lg text-center">We give you an overview of all the features so you can decide weather to use the tool or not</p>
-            <div className="flex gap-12 mt-8">
-                <div className="w-[320px] h-[320px] bg-gray-200"></div>
-                <div className="w-[320px] h-[320px] bg-red-300"></div>
+      <section className="relative bg-mantis-300">
+        <div className="container py-12 flex flex-col lg:flex-row">
+          <div className="max-w-xl">
+            <span className="bg-mantis-100 border border-mantis-800 rounded-full py-1 px-4 text-sm font-medium text-mantis-800">
+              Precision in Customization
+            </span>
+            <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mt-2 mb-6 text-mantis-950">
+              <Balancer>
+                Enhance Your Project with Effortless Refinement of Waitlist
+                Forms.
+              </Balancer>
+            </h2>
+            <p className="text-xl">
+              <Balancer>
+                Effortlessly Tailor Your Waitlist Form for a Captivating User
+                Experience. Dive into Seamless Customization with Our Pre-launch
+                Forms, Designed to Delight.
+              </Balancer>
+            </p>
+            <div className="mt-6">
+              <ul className="">
+                <li className="flex items-center mb-2">
+                  <svg
+                    className="w-4 h-4 mr-2 text-green-500"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 13l4 4L19 7"></path>
+                  </svg>
+                  Customize forms effortlessly for your project's unique style.
+                </li>
+                <li className="flex items-center mb-2">
+                  <svg
+                    className="w-4 h-4 mr-2 text-green-500"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 13l4 4L19 7"></path>
+                  </svg>
+                  Integrate seamlessly with other tools using our accessible
+                  API.
+                </li>
+                <li className="flex items-center">
+                  <svg
+                    className="w-4 h-4 mr-2 text-green-500"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 13l4 4L19 7"></path>
+                  </svg>
+                  Collect specific user data with customizable fields.
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="relative mt-6 md:mt-0 w-full h-[320px] md:h-[550px]">
+            <Image src={customise} alt="Customise" fill/>
+          </div>
+        </div>
+      </section>
+      <section className="bg-mantis-100 py-16">
+        <div className="container flex flex-col items-center">
+            <h2 className={cn("text-2xl sm:text-3xl md:text-4xl font-bold text-mantis-800 mb-4", playfaire.className)}>
+                Join the waitlist
+            </h2>
+            <p className="mb-2">Get early access by joining the waitlist</p>
+            <div className="max-w-sm w-full h-[260px] overflow-hidden">
+                <iframe src={`${url}/w/e/bk`} width="100%" height="100%"/>
             </div>
         </div>
-      </section> */}
+      </section>
+      <footer className="bg-mantis-900 py-12">
+        <div className="container">
+            <div>
+            <div className="flex items-center md:gap-2">
+            <PackageCheck className="text-mantis-50 text-5xl" />
+            <span className="text-xl md:text-2xl text-mantis-50 font-bold">
+              waitwisehub
+            </span>
+          </div>
+          <p className="text-mantis-50 ">waitwisehub - supercharge your launch</p>
+            </div>
+            <div className="my-4 py-4 border-t border-t-mantis-50 text-mantis-50">
+                &copy; {(new Date()).getFullYear()} waitwisehub
+            </div>
+        </div>
+      </footer>
     </React.Fragment>
   );
 };
