e76e4f1c6296d01b198a2ad95520f2db6e462e12
Syed Muzamil <smmhd121@gmail.com>
2024-01-14 18:11:24 +0530

    Removed the subscription logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,65 +10,65 @@ export async function middleware(request: NextRequest) {
     },
   })
 
-  const supabase = createServerClient(
-    supabaseUrl,
-    supabaseAnonKey,
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-        },
-        remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
-        },
-      },
-    }
-  )
+  // const supabase = createServerClient(
+  //   supabaseUrl,
+  //   supabaseAnonKey,
+  //   {
+  //     cookies: {
+  //       get(name: string) {
+  //         return request.cookies.get(name)?.value
+  //       },
+  //       set(name: string, value: string, options: CookieOptions) {
+  //         request.cookies.set({
+  //           name,
+  //           value,
+  //           ...options,
+  //         })
+  //         response = NextResponse.next({
+  //           request: {
+  //             headers: request.headers,
+  //           },
+  //         })
+  //         response.cookies.set({
+  //           name,
+  //           value,
+  //           ...options,
+  //         })
+  //       },
+  //       remove(name: string, options: CookieOptions) {
+  //         request.cookies.set({
+  //           name,
+  //           value: '',
+  //           ...options,
+  //         })
+  //         response = NextResponse.next({
+  //           request: {
+  //             headers: request.headers,
+  //           },
+  //         })
+  //         response.cookies.set({
+  //           name,
+  //           value: '',
+  //           ...options,
+  //         })
+  //       },
+  //     },
+  //   }
+  // )
 
-  const {data: { session }} = await supabase.auth.getSession()
+  // const {data: { session }} = await supabase.auth.getSession()
 
-  const url = new URL(request.url)
-  if (session) {
-    const { data: subscription } =  await supabase.from('subscriptions').select('*').eq('user_id', session?.user.id).single().throwOnError()
-    if (!subscription || subscription.status !== "active") {
-        if (!request.url.includes('account')) {
-          return NextResponse.redirect(`${url.origin}/dashboard/account`)
-        }
-    }
-  } else {
-    return NextResponse.redirect(`${url.origin}/signup`)
-  }
+  // const url = new URL(request.url)
+  // if (session) {
+  //   const { data: subscription } =  await supabase.from('subscriptions').select('*').eq('user_id', session?.user.id).single().throwOnError()
+  //   if (!subscription || subscription.status !== "active") {
+  //       if (!request.url.includes('account')) {
+  //         return NextResponse.redirect(`${url.origin}/dashboard/account`)
+  //       }
+  //   }
+  // } else {
+  //   return NextResponse.redirect(`${url.origin}/signup`)
+  // }
 
   return response
 }
