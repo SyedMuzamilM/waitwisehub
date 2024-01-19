8c0f8b999c6f7c78b690360c623b5f7762142c94
Syed Muzamil <smmhd121@gmail.com>
2024-01-19 10:45:13 +0530

    Fixed the headers

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/v1/:path*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
@@ -21,22 +21,22 @@ const nextConfig = {
           },
         ],
       },
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: 'https://waitwisehub.blackkalu.com' }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
+      // {
+      //   source: "/api/:path*",
+      //   headers: [
+      //     { key: "Access-Control-Allow-Credentials", value: "true" },
+      //     { key: "Access-Control-Allow-Origin", value: 'https://waitwisehub.blackkalu.com' }, // replace this your actual origin
+      //     {
+      //       key: "Access-Control-Allow-Methods",
+      //       value: "GET,DELETE,PATCH,POST,PUT",
+      //     },
+      //     {
+      //       key: "Access-Control-Allow-Headers",
+      //       value:
+      //         "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+      //     },
+      //   ],
+      // },
     ];
   },
 };
