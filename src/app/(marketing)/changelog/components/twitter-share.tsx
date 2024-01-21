b4b4084c972a8350b74d69f4f32914e8e39fa21d
Syed Muzamil <smmhd121@gmail.com>
2024-01-21 19:50:20 +0530

    New home page and changelogs page

---
+++ src/app/(marketing)/changelog/components/twitter-share.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const TwitterShare = ({ title, slug }: { title: string; slug: string; }) => {
+  const text = `Read about "${title}" in this changelog.`
+  return (
+    <div>
+      Share this changelog
+      <a
+        target="_blank"
+        rel="noopener noreferer"
+        className="max-w-max flex items-center hover:text-dark-muted transition-colors duration-150 ease-in-out mt-2.5 rounded outline-1 outline-offset-2 outline-dark-control-base focus-visible:outline"
+        href={`https://x.com/intent/tweet?text=${text}.%0A%0A%0A%E2%86%92%20https://waitwisehub.blackkalu.com/changelog/${slug}`}
+      >
+        <svg
+          className="w-4 mr-2.5"
+          viewBox="0 0 21.57 19.5"
+          width="21.57"
+          height="19.5"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M16.99 0h3.308l-7.227 8.26 8.502 11.24h-6.657l-5.214-6.817L3.736 19.5H.426l7.73-8.835L0 0h6.826l4.713 6.231L16.99 0Zm-1.161 17.52h1.833L5.83 1.876H3.863L15.829 17.52Z"
+            fill="currentColor"
+          ></path>
+        </svg>
+        Write a tweet
+      </a>
+    </div>
+  );
+};
--- src/app/(marketing)/components/hero-section.tsx
+++ src/app/(marketing)/components/hero-section.tsx
@@ -2,17 +2,18 @@ import React from "react";
 import { Playfair_Display } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Balancer from "react-wrap-balancer";
+import IFrame from "./iframe";
 
 const playfaire = Playfair_Display({ subsets: ["latin"] });
 
 export const HeroSection = () => {
   return (
-    <section className="-bg-brand/60 py-16">
+    <section className="bg-black pb-16 pt-32">
       <div className="container flex flex-col items-center gap-5 max-w-[64rem]">
         <div className="mb-12 flex flex-col items-center text-center gap-5">
           <h1
             className={cn(
-              "text-4xl font-extrabold tracking-light sm:text-5xl md:text-6xl lg:text-7xl text-mantis-800",
+              "text-4xl font-extrabold tracking-light sm:text-5xl md:text-6xl lg:text-7xl text-white",
               playfaire.className
             )}
           >
@@ -20,7 +21,7 @@ export const HeroSection = () => {
               WaitwiseHub! */}
             <Balancer>WaitWiseHub - Supercharge Your Launch</Balancer>
           </h1>
-          <p className="text-mantis-800 max-w-[42rem] leading-normal text-muted-foreground sm:text-sm md:text-xl sm:leading-0">
+          <p className="max-w-[42rem] leading-normal text-gray-400 sm:text-sm md:text-xl sm:leading-0">
             <Balancer>
               Transform Waiting into Winning – Elevate Your Startup! 🚀 Join
               WaitwiseHub, the Ultimate Waitlist Platform. Focus on Perfecting
@@ -28,14 +29,18 @@ export const HeroSection = () => {
             </Balancer>
           </p>
         </div>
-        <div className="flex gap-4 md:gap-8">
+        <div className="text-center space-y-4">
+          <p className="text-xl font-medium text-gray-300">Join our waitlist</p>
+          <IFrame />
+        </div>
+        {/* <div className="flex gap-4 md:gap-8">
           <button className="bg-mantis-900 hover:bg-mantis-950 transition-colors shadow-md rounded-full py-3 px-4 md:py-3 md:px-6 text-white font-medium">
             Build your waitlist
           </button>
           <button className="bg-mantis-100 hover:bg-mantis-200 transition-colors rounded-full py-3 px-4 md:py-3 md:px-6 text-brand-dark font-medium">
             View demo
           </button>
-        </div>
+        </div> */}
       </div>
     </section>
   );
