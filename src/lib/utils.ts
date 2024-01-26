66d357a52e50fa759b48d31f5b1f8da25681748c
Syed Muzamil <smmhd121@gmail.com>
2024-01-26 15:15:37 +0530

    Added some country constants and userful utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,57 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+interface SWRError extends Error {
+  status: number;
+}
+
+export async function fetcher<JSON = any>(
+  input: RequestInfo,
+  init?: RequestInit,
+): Promise<JSON> {
+  const res = await fetch(input, init);
+
+  if (!res.ok) {
+    const error = await res.text();
+    const err = new Error(error) as SWRError;
+    err.status = res.status;
+    throw err;
+  }
+
+  return res.json();
+}
+
+export function nFormatter(
+  num?: number,
+  opts: { digits?: number; full?: boolean } = {
+    digits: 1,
+  },
+) {
+  if (!num) return "0";
+  if (opts.full) {
+    return Intl.NumberFormat("en-US").format(num);
+  }
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "K" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" },
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  var item = lookup
+    .slice()
+    .reverse()
+    .find(function (item) {
+      return num >= item.value;
+    });
+  return item
+    ? (num / item.value).toFixed(opts.digits).replace(rx, "$1") + item.symbol
+    : "0";
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
